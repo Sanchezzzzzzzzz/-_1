26f9fe8af3798b54defa0cda178100d12e4f6ed6
Alexandra <alexadudki11@gmail.com>
2023-07-15 05:13:10 +0300

    Получилось, но почему-то два раза печатается

--- Program.cs
+++ Program.cs
@@ -8,15 +8,13 @@ void InputArr(string[] array, int length){
 }
 void PrintNewArr(string[] array, int length){
     for(int i = 0; i < length; i++){
-        if(array[i].Length<=3){
-            Console.WriteLine(array[i]+"");
-        }
+        Console.WriteLine(string.Join(",", array.Where(array => array.Length<=3)));
     }
 }
 Write("Введите размер массива");
 int length=int.Parse(Console.ReadLine()!);
 Write("Введите массив");
-Console.WriteLine();
 string[] array = new string[length];
 InputArr(array, length);
+Console.WriteLine();
 PrintNewArr(array, length);
