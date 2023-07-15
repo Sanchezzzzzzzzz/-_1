bb553357194ed182765bf400c622434001907a39
Alexandra <alexadudki11@gmail.com>
2023-07-15 04:56:39 +0300

    написала то, что пришло пока в голову

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Write(string message){
+    Console.WriteLine(message);
+}
+Write("Введите размер массива");
+int length=int.Parse(Console.ReadLine()!);
+Write("Введите массив");
+string[] array = new string[length];
+InputArr(array, length);
+PrintNewArr(array, length);
+void InputArr(string[] array, int length){
+    for(int i = 0; i < length; i++){
+        array[i]=Console.ReadLine()!;
+    }
+}
+void PrintNewArr(string[] array, int length){
+    for(int i = 0; i < length; i++){
+        if(array[i].Length<=3){
+            Console.WriteLine(array[i]);
+            Console.WriteLine();
+        }
+    }
+}
